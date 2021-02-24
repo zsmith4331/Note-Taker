3e37cd12c65449c942cc860e79ee7ff05570c5b8
Zach Smith <zsmith4331@gmail.com>
2021-02-24 00:28:51 -0500

    updated server.js with functions and routes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // Dependencies //
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 // Express Configuration //
 const app = express();
@@ -8,13 +10,52 @@ const PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing //
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-// Route files //
-require("./routes/apiRoutes")(app); 
-require("./routes/htmlRoutes")(app); 
+fs.readFile("db/db.json","utf8", (error, data) => {
 
-// Listener //
-app.listen(PORT, () => {
-    console.log(`App listening on PORT: ${PORT}`);
-  });
-  
+    if (error) throw error;
+    
+    var notes = JSON.parse(data);
+
+    // API Route to send notes saved in JSON format //
+    app.get("/api/notes", (request, response) => response.json(notes));
+
+    // API Route to grab notes with a particular ID //
+    app.get("/api/notes/:id", (request, response) => response.json(notes[request.params.id]));
+
+    // Updates & saves new note //
+    app.post("/api/notes", (request, response) => {
+        let newNote = request.body;
+        notes.push(newNote);
+        console.log("Adding new note tilted: " + newNote.title)
+        updateDB();
+        return response.json({});
+    });
+
+    // Deletes a note with a particular ID //
+    app.delete("/api/notes/:id", (request, response) => {
+        notes.splice(request.params.id, 1);
+        updateDB();
+        return console.log("Deleting note ID: " + request.params.id);
+    })
+      
+    // Displays notes.html //
+    app.get('/notes', (request,response) => response.sendFile(path.join(__dirname, "./public/notes.html")));
+
+    // Displays index.html //
+    app.get('*', (request,response) => response.sendFile(path.join(__dirname, "./public/index.html")));
+ 
+    // Updates json file when a note is added or deleted //
+    function updateDB() {
+        fs.writeFile("db/db.json",JSON.stringify(notes,'\t'), error => {
+
+            if (error) throw error;
+            return true;
+
+        });
+    }
+});
+
+// Starts the server to begin listening //
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
